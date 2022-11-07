1e93d9593912435a9b711d4de632b47d4208d647
Daniel Weiner <info@phantomnet.net>
2022-11-06 22:08:40 -0800

    Macros: define fusion specific macros

---
+++ libraries/core/source/public/Fusion/Macros.h
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2015-2022 Daniel Weiner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#pragma once
+
+#include <Fusion/Compiler.h>
+
+#if FUSION_HAS_EXCEPTIONS
+#define FUSION_NOEXCEPT noexcept
+#else
+#define FUSION_NOEXCEPT
+#endif
+
+#define _FUSION_WIDE(str) L ## str
+#define FUSION_WIDE(str) _FUSION_WIDE(str)
+
+namespace Fusion
+{
+    struct Ignored
+    {
+        template<typename T>
+        constexpr const Ignored& operator=(const T&) const noexcept
+        {
+            // Does nothing
+            return *this;
+        }
+    };
+
+    constexpr Ignored Ignore{};
+}  // namespace Fusion
+
+#define FUSION_UNUSED(x) Fusion::Ignore = x;
