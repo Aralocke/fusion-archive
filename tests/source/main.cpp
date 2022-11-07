9a65af6bbf2990bed4bfb92dec3cd3a7985cac82
Daniel Weiner <info@phantomnet.net>
2022-11-06 21:30:40 -0800

    FusionTests: new unit test project for testing the Fusion components

---
+++ tests/source/main.cpp
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2015-2022 Daniel Weiner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#include <Fusion/Tests/Main.h>
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv)
+{
+    using namespace Fusion;
+
+    // SetDefaultLogLevel(fusion::LogLevel::LOG_NOISE);
+    // DefaultLogger()->AddHandler(
+    //    std::make_shared<ConsoleLogHandler>(std::cout));
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
