968c2a317a1cce87ce6bfa36230fca5ccd9e9445
Daniel Weiner <info@phantomnet.net>
2023-04-23 21:53:02 -0700

    IOCP SocketService WIP

---
+++ libraries/core/source/platforms/windows/IocpSocketService.cpp
@@ -0,0 +1,79 @@
+/**
+ * Copyright 2015-2022 Daniel Weiner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#include <Fusion/Internal/IocpSocketService.h>
+
+#if FUSION_PLATFORM_WINDOWS
+namespace Fusion::Internal
+{
+IocpSocketService::IocpSocketService(Network& network)
+    : m_network(network)
+    , m_pipe(network)
+{ }
+
+IocpSocketService::~IocpSocketService()
+{
+}
+
+Result<void> IocpSocketService::Add(
+    Socket sock,
+    SocketOperation operation)
+{
+    return Failure{ E_NOT_IMPLEMENTED };
+}
+
+Result<void> IocpSocketService::Close(Socket sock)
+{
+    return Failure{ E_NOT_IMPLEMENTED };
+}
+
+void IocpSocketService::Notify()
+{}
+
+Result<std::span<SocketEvent>>
+IocpSocketService::Execute(Clock::duration timeout)
+{
+    return Failure{ E_NOT_IMPLEMENTED };
+}
+
+Result<void> IocpSocketService::Remove(
+    Socket sock,
+    SocketOperation events)
+{
+    return Failure{ E_NOT_IMPLEMENTED };
+}
+
+Result<void> IocpSocketService::Start()
+{
+    return Failure{ E_NOT_IMPLEMENTED };
+}
+
+void IocpSocketService::Stop()
+{
+    Stop(nullptr);
+}
+
+void IocpSocketService::Stop(std::function<void(Failure&)> fn)
+{
+    if (fn)
+    {
+        Failure f(E_SUCCESS);
+
+        fn(f);
+    }
+}
+}  // namespace Fusion::Internal
+#endif  // FUSION_PLATFORM_WINDOWS
---
+++ libraries/core/source/private/Fusion/Internal/IocpSocketService.h
@@ -0,0 +1,87 @@
+/**
+ * Copyright 2015-2022 Daniel Weiner
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+#pragma once
+
+#include <Fusion/Windows.h>
+#if FUSION_PLATFORM_WINDOWS
+
+#include <Fusion/Internal/Network.h>
+
+namespace Fusion::Internal
+{
+//
+//
+//
+class IocpSocketService final
+    : public SocketService
+{
+public:
+    IocpSocketService(Network& network);
+    ~IocpSocketService() override;
+
+public:
+    //
+    //
+    //
+    Result<void> Add(
+        Socket sock,
+        SocketOperation events) override;
+
+    //
+    //
+    //
+    Result<void> Close(Socket sock) override;
+
+    //
+    //
+    //
+    void Notify() override;
+
+    //
+    //
+    //
+    Result<std::span<SocketEvent>> Execute(Clock::duration timeout) override;
+
+    //
+    //
+    //
+    Result<void> Remove(
+        Socket sock,
+        SocketOperation events) override;
+
+    //
+    //
+    //
+    Result<void> Start() override;
+
+    //
+    //
+    //
+    void Stop() override;
+
+    //
+    //
+    //
+    void Stop(std::function<void(Failure&)> fn) override;
+
+private:
+    Network& m_network;
+    SocketPair m_pipe;
+};
+}  // namespace Fusion::Internal
+
+#endif  // FUSION_PLATFORM_WINDOWS
